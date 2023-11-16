3e86cb21400b7227c2ca63c04836320b11c2f49c
Lipan Ioan <lipanioan420@gmail.com>
2023-11-16 11:13:19 +0200

    move all components to different files

---
+++ src/components/FriendsList.js
@@ -0,0 +1,16 @@
+import { Friend } from "./Friend";
+
+export function FriendsList({ friends, onSelection, selectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          selectedFriend={selectedFriend}
+          onSelection={onSelection}
+        />
+      ))}
+    </ul>
+  );
+}
